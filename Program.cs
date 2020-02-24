b13f102b3ac7cff62d642f5f8046cc2be5ffecd4
Seth T Barrett <54868870+sethbarrett50@users.noreply.github.com>
2020-02-24 15:13:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//This is HW2 Pt2 for Professor JBara's PCP2 Class by Seth Barrett
+//This is a linked list of cards
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList L = new LinkedList();
+          
+            L.AddFirst(5, 'C');
+            L.AddFirst(3, 'D');
+            L.AddFirst(8, 'S');
+           
+        }
+    }
+}
